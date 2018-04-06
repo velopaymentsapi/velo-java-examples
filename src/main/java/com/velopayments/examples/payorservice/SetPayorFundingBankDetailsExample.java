f222879c96510269aba2256b1ef0da5d983cfe66
John Thompson <john@springframework.guru>
2018-04-06 13:00:18 -0400

    incremental

---
+++ src/main/java/com/velopayments/examples/payorservice/SetPayorFundingBankDetailsExample.java
@@ -0,0 +1,32 @@
+package com.velopayments.examples.payorservice;
+
+import com.velopayments.examples.authorization.AuthorizationExample;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+
+/**
+ * Created by jt on 3/29/18.
+ */
+public class SetPayorFundingBankDetailsExample {
+
+
+    public static void main(String[] args) throws IOException {
+        String apiUrl = "https://api.sandbox.velopayments.com/v1/payors/{payorId}/payorFundingBankDetailsUpdate";
+
+        //Payor ID - Unique to your account
+        String payorId = "61e0690e-7d3f-4f87-8740-cf87565369d0";
+
+        //Get API Access Token
+        String apiAccessToken = AuthorizationExample.getApiToken();
+
+        // Query parameters
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(apiUrl)
+                // Add query parameter
+                .queryParam("payorId", payorId);
+
+        String apiUrlWithQueryParams = builder.toUriString();
+
+        System.out.println("API URL with query Parameters: " + apiUrlWithQueryParams);
+    }
+}
